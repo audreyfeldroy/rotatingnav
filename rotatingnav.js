e1c7a1b7a6bc5a6740844dc3c2ef4490cfdb9b0d
Audrey Roy <audreyr@gmail.com>
2013-08-04 12:09:40 +0200

    Partial checkin - cleanup, renaming to avoid conflict, bringing in less.

--- rotatingnav.js
+++ rotatingnav.js
@@ -111,7 +111,7 @@
 
     $next = $next.length ? $next : this.$element.find('.item')[fallback]()
 
-    var e = $.Event('slide.bs.carousel', { relatedTarget: $next[0], direction: direction })
+    var e = $.Event('slide.bs.rotatingnav', { relatedTarget: $next[0], direction: direction })
 
     if ($next.hasClass('active')) return
 
@@ -163,30 +163,30 @@
   // CAROUSEL PLUGIN DEFINITION
   // ==========================
 
-  var old = $.fn.carousel
+  var old = $.fn.rotatingnav
 
-  $.fn.carousel = function (option) {
+  $.fn.rotatingnav = function (option) {
     return this.each(function () {
       var $this   = $(this)
-      var data    = $this.data('bs.carousel')
+      var data    = $this.data('bs.rotatingnav')
       var options = $.extend({}, Carousel.DEFAULTS, $this.data(), typeof option == 'object' && option)
       var action  = typeof option == 'string' ? option : options.slide
 
-      if (!data) $this.data('bs.carousel', (data = new Carousel(this, options)))
+      if (!data) $this.data('bs.rotatingnav', (data = new Carousel(this, options)))
       if (typeof option == 'number') data.to(option)
       else if (action) data[action]()
       else if (options.interval) data.pause().cycle()
     })
   }
 
-  $.fn.carousel.Constructor = Carousel
+  $.fn.rotatingnav.Constructor = Carousel
 
 
   // CAROUSEL NO CONFLICT
   // ====================
 
-  $.fn.carousel.noConflict = function () {
-    $.fn.carousel = old
+  $.fn.rotatingnav.noConflict = function () {
+    $.fn.rotatingnav = old
     return this
   }
 
@@ -194,26 +194,26 @@
   // CAROUSEL DATA-API
   // =================
 
-  $(document).on('click.bs.carousel.data-api', '[data-slide], [data-slide-to]', function (e) {
+  $(document).on('click.bs.rotatingnav.data-api', '[data-slide], [data-slide-to]', function (e) {
     var $this   = $(this), href
     var $target = $($this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) //strip for ie7
     var options = $.extend({}, $target.data(), $this.data())
     var slideIndex = $this.attr('data-slide-to')
     if (slideIndex) options.interval = false
 
-    $target.carousel(options)
+    $target.rotatingnav(options)
 
     if (slideIndex = $this.attr('data-slide-to')) {
-      $target.data('bs.carousel').to(slideIndex)
+      $target.data('bs.rotatingnav').to(slideIndex)
     }
 
     e.preventDefault()
   })
 
   $(window).on('load', function () {
-    $('[data-ride="carousel"]').each(function () {
-      var $carousel = $(this)
-      $carousel.carousel($carousel.data())
+    $('[data-ride="rotatingnav"]').each(function () {
+      var $rotatingnav = $(this)
+      $rotatingnav.rotatingnav($rotatingnav.data())
     })
   })
 
