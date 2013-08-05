df2a1a6eb64d549ba5b4852ff64cde8386fef49b
Audrey Roy <audreyr@gmail.com>
2013-08-05 10:07:22 +0200

    expand -t 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,69 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-		// Import package manifest
-		pkg: grunt.file.readJSON("rotatingnav.jquery.json"),
+    // Import package manifest
+    pkg: grunt.file.readJSON("rotatingnav.jquery.json"),
 
-		// Banner definitions
-		meta: {
-			banner: "/*\n" +
-				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
-				" *  <%= pkg.description %>\n" +
-				" *  <%= pkg.homepage %>\n" +
-				" *\n" +
-				" *  Made by <%= pkg.author.name %>\n" +
-				" *  Under <%= pkg.licenses[0].type %> License\n" +
-				" */\n"
-		},
+    // Banner definitions
+    meta: {
+      banner: "/*\n" +
+        " *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+        " *  <%= pkg.description %>\n" +
+        " *  <%= pkg.homepage %>\n" +
+        " *\n" +
+        " *  Made by <%= pkg.author.name %>\n" +
+        " *  Under <%= pkg.licenses[0].type %> License\n" +
+        " */\n"
+    },
 
-		// Concat definitions
-		concat: {
-			dist: {
-				src: ["src/jquery.rotatingnav.js"],
-				dest: "dist/jquery.rotatingnav.js"
-			},
-			options: {
-				banner: "<%= meta.banner %>"
-			}
-		},
+    // Concat definitions
+    concat: {
+      dist: {
+        src: ["src/jquery.rotatingnav.js"],
+        dest: "dist/jquery.rotatingnav.js"
+      },
+      options: {
+        banner: "<%= meta.banner %>"
+      }
+    },
 
-		// Lint definitions
-		jshint: {
-			files: ["src/jquery.rotatingnav.js"],
-			options: {
-				jshintrc: ".jshintrc"
-			}
-		},
+    // Lint definitions
+    jshint: {
+      files: ["src/jquery.rotatingnav.js"],
+      options: {
+        jshintrc: ".jshintrc"
+      }
+    },
 
-		// Minify definitions
-		uglify: {
-			my_target: {
-				src: ["dist/jquery.rotatingnav.js"],
-				dest: "dist/jquery.rotatingnav.min.js"
-			},
-			options: {
-				banner: "<%= meta.banner %>"
-			}
-		},
+    // Minify definitions
+    uglify: {
+      my_target: {
+        src: ["dist/jquery.rotatingnav.js"],
+        dest: "dist/jquery.rotatingnav.min.js"
+      },
+      options: {
+        banner: "<%= meta.banner %>"
+      }
+    },
 
-		// Less compilation
-		less: {
-			compile: {
-				files: {
-					"dist/jquery.rotatingnav.css": "src/rotatingnav.less"
-				}
-			}
-		}
+    // Less compilation
+    less: {
+      compile: {
+        files: {
+          "dist/jquery.rotatingnav.css": "src/rotatingnav.less"
+        }
+      }
+    }
 
-	});
+  });
 
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-less");
 
-	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
-	grunt.registerTask("travis", ["jshint"]);
+  grunt.registerTask("default", ["jshint", "concat", "uglify"]);
+  grunt.registerTask("travis", ["jshint"]);
 
 };
